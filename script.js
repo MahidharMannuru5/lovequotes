fca66eda2dbb16652e418b4986e5835000bf4069
Mahidhar Mannuru <81514511+MahidharMannuru5@users.noreply.github.com>
2021-07-23 12:56:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ async function generateJoke() {
     },
   }
 
-  const res = await fetch('https://icanhazdadjoke.com', config)
+  const res = await fetch('https://www.thecalculator.co/', config)
 
   const data = await res.json()
 
